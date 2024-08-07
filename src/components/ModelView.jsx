861798321add74a3819f6d51967088dad95dbe51
Amon Mihaljcic <amon.mihaljcic03@gmail.com>
2024-08-07 12:41:56 +0200

    Added Phone rotation and customization

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -4,14 +4,16 @@ import Lights from './Lights'
 import IPhone from './IPhone'
 import { Suspense } from 'react'
 
-import * as THREE from 'three'
 
-const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
+import * as THREE from 'three'
+import Loader from './Loader'
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
     <View
     index={index}
     id={gsapType}
-    className={`w-full h-full $ {index === 2} ? right-[-100%] : ''`}
+    className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
     >
       {/* Ambient Light */}
       <ambientLight intensity={0.3} />
@@ -32,7 +34,7 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, siz
 
       
       <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0, 0]}>
-        <Suspense fallback={<div>Loading</div>}>
+        <Suspense fallback={<Loader />}>
           <IPhone
           scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
           item={item}
